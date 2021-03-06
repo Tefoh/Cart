0f74be3f6fafadce8b1b1f0e83f66f83c299c8b0
Tefoh <tefo.ha27@gmal.com>
2021-03-06 19:26:27 +0330

    fix config

--- src/CartServiceProvider.php
+++ src/CartServiceProvider.php
@@ -14,6 +14,9 @@ class CartServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind('cart', 'Tefo\\Cart\\Cart');
+        $config = __DIR__ . '/../config/cart.php';
+        $this->mergeConfigFrom($config, 'cart');
+        $this->publishes([__DIR__ . '/../config/cart.php' => config_path('cart.php')], 'config');
 
         $this->app['events']->listen(Logout::class, function () {
             if ($this->app['config']->get('cart.destroy_on_logout')) {
