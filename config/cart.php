02dfc1c596b1f145ca851f59f9db8fc3969c1013
Tefoh <tefo.ha27@gmal.com>
2021-03-06 17:45:00 +0330

    add config

---
+++ config/cart.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default tax rate
+    |--------------------------------------------------------------------------
+    |
+    | This default tax rate will be used when you make a class implement the
+    | Taxable interface and use the HasTax trait.
+    |
+    */
+
+    'tax' => 21,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Destroy the cart on user logout
+    |--------------------------------------------------------------------------
+    |
+    | When this option is set to 'true' the cart will automatically
+    | destroy all cart instances when the user logs out.
+    |
+    */
+
+    'destroy_on_logout' => false,
+
+    // default number formats
+
+    'format' => [
+        'decimals' => 2,
+        'decimal_point' => '.',
+        'thousand_separator' => ','
+    ],
+];
